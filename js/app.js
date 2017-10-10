2aeca0e36c48e3b34cb5ef28131fd059d4c56767
mcjc29 <marthacarmen@hotmail.co.uk>
2017-10-10 11:14:47 +0100

    complex expression evaluated

--- js/app.js
+++ js/app.js
@@ -33,70 +33,73 @@ function operator(){
 function subExpression() {
   return `${emptyOrNot()} ${tvVal()} ${operator()} ${emptyOrNot()} ${tvVal()}`;
 }
-// console.log(subExpression());
+  console.log(subExpression());
 
 function answersubEx() {
   return eval(subExpression());
 }
-// console.log(answersubEx());
 
-function randomNumber(isOdd, min, max){
-  let num = Math.floor(Math.random() * (max - min) +min);
-  if (isOdd && num % 2 === 0) num +=1;
-  if (!isOdd && num % 2 === 1) num +=1;
-  // console.log(num);
+  console.log(answersubEx());
+
+function randomNumber(isOdd, min, max) {
+  let num = Math.ceil(Math.random() * (max - min) + min);
+  // console.log(`After generating random number: ${num}`);
+  if (isOdd) {
+    if (num % 2 === 0) num++;
+  } else {
+    if (num % 2 !== 0) num++;
+  }
+  // else if (num % 2 !== 0) num;
+  // console.log(`After checking odd / even: ${num}`);
   return num;
 }
 
+// console.log(randomNumber());
+// if (i === 0 || isOdd && num % 2 === 0) num +=1;
+// if math.ceil( then to smallest num define)
+// define i
 function generateBracketPair(arr) {
-  const openIndex = randomNumber(false, 0, arr.length -1);
-  const closeIndex = randomNumber(false, openIndex +1, arr.length -1);
+  const lastItem = arr.length - 2;
+  const openIndex = randomNumber(false, 0, arr.length - 4);
+  const closeIndex = randomNumber(false, openIndex + 1, lastItem);
   //arr.splice(openIndex, 0, openBracket;
+  // console.log(arr);
+  // console.log(`openindex: ${openIndex}, closeIndex: ${closeIndex}`);
   arr[openIndex] = openBracket.concat(arr[openIndex]);
-  arr[closeIndex] = arr[closeIndex].concat(closeBracket);
-  console.log(arr[openIndex]);
-  console.log(arr[closeIndex]);
+  arr[closeIndex] = (arr[closeIndex]).concat(closeBracket);
+
+        console.log(eval(arr.join(' ')));
+  // console.log(`what is open ${arr[openIndex]}`);
+  // console.log(`what is close ${arr[closeIndex]}`);
   return arr;
 }
 
 function generateExp() {
   var arraySubEx = [];
-  const arrayLength = randomNumber(true, 1, 20);
+  // const arrayLength = randomNumber(true, 1, 11);
+  const arrayLength = randomNumber(true, 3, 11);
   for (var i = 0; i < arrayLength; i++) {
     if (i % 2 === 0) {
       arraySubEx.push(subExpression());
-    } else arraySubEx.push(operator());
-
+    } else {
+      arraySubEx.push(operator());
+    }
     //arraySubEx.push(i % 2 === 0 ? subExpression() : operator());
   }
-  const numberOfPairs = Math.random() * (arrayLength/2);
+  const numberOfPairs = Math.random() * (arrayLength / 2);
   for(i =0; i< numberOfPairs; i++){
     arraySubEx = generateBracketPair(arraySubEx);
   }
   return arraySubEx;
 }
 
-generateExp();
-console.log(generateExp());
-//
+  // console.log(generateExp());
 
 
-// console.log(arraySubEx);
+function ansGenerateExp() {
+  return eval(generateExp());
+}
+ansGenerateExp();
+  // console.log(ansGenerateExp());
 
-
-
-
-// console.log(bracketed());
-
-// ensure no empty array is spat out & operator not on end
-
-// // //
-// function complexExp() {
-//   return `${arraySubEx}, ${operator()}`;
-// }
-// console.log(complexExp());
-//
-// function answerComplexExp() {
-//   return eval(subExpression());
-// }
-// console.log(answerComplexExp());
+// if starts and end with backets randomly assign!
