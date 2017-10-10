0c88c1f94528272e167c126b940d423a672d33f8
mcjc29 <marthacarmen@hotmail.co.uk>
2017-10-10 14:51:17 +0100

    styling begins

--- js/app.js
+++ js/app.js
@@ -2,23 +2,45 @@
 Expression => Whole thing
 ((Sub-expression) && Sub-expression) || Sub-expression
 Length (Number of sub-expressions in the total expression)
-
 Array = [sub-expression, sub-expression, sub-expression]
 Add bracket
 Final = sub-expression + '&&'
 Add bracket !
 choose arbitarily say max 5 expressions
 either bracket all inc ! or brack subs in || &&
-
 calculate expressions along the way on submit button
 if subexpression = 0 the change html to false
 */
+
+let $problem = null;
+let $stats = null;
+let $submit  = null;
+let $input = null;
+// let currentExpression = null;
+let stats = 0;
 
 const operators = ['||', '&&'];
 const emptyOrNor = ['', '!'];
 const tValue = [true, false];
 const openBracket = '(';
 const closeBracket = ')';
+
+$(init);
+
+function init() {
+  $problem       = $('.problem');
+  $stats         = $('.stats');
+  $submit        = $('input[type="submit"]');
+  $input         = $('input[type="text"]');
+
+  $('button').on('click', startGame);
+
+}
+
+function startGame() {
+  generateExp();
+  $submit.on('click', checkAnswer());
+}
 
 function emptyOrNot(){
   return `${emptyOrNor[Math.floor(emptyOrNor.length * Math.random())]}`;
@@ -33,31 +55,21 @@ function operator(){
 function subExpression() {
   return `${emptyOrNot()} ${tvVal()} ${operator()} ${emptyOrNot()} ${tvVal()}`;
 }
-  console.log(subExpression());
-
-function answersubEx() {
-  return eval(subExpression());
-}
-
-  console.log(answersubEx());
+// function answersubEx() {
+//   return eval(subExpression());
+// }
+// // console.log(answersubEx());
 
 function randomNumber(isOdd, min, max) {
   let num = Math.ceil(Math.random() * (max - min) + min);
-  // console.log(`After generating random number: ${num}`);
   if (isOdd) {
     if (num % 2 === 0) num++;
   } else {
     if (num % 2 !== 0) num++;
   }
-  // else if (num % 2 !== 0) num;
-  // console.log(`After checking odd / even: ${num}`);
   return num;
 }
 
-// console.log(randomNumber());
-// if (i === 0 || isOdd && num % 2 === 0) num +=1;
-// if math.ceil( then to smallest num define)
-// define i
 function generateBracketPair(arr) {
   const lastItem = arr.length - 2;
   const openIndex = randomNumber(false, 0, arr.length - 4);
@@ -67,8 +79,7 @@ function generateBracketPair(arr) {
   // console.log(`openindex: ${openIndex}, closeIndex: ${closeIndex}`);
   arr[openIndex] = openBracket.concat(arr[openIndex]);
   arr[closeIndex] = (arr[closeIndex]).concat(closeBracket);
-
-        console.log(eval(arr.join(' ')));
+  // console.log(eval(arr.join(' ')));
   // console.log(`what is open ${arr[openIndex]}`);
   // console.log(`what is close ${arr[closeIndex]}`);
   return arr;
@@ -77,7 +88,7 @@ function generateBracketPair(arr) {
 function generateExp() {
   var arraySubEx = [];
   // const arrayLength = randomNumber(true, 1, 11);
-  const arrayLength = randomNumber(true, 3, 11);
+  const arrayLength = randomNumber(true, 2, 11);
   for (var i = 0; i < arrayLength; i++) {
     if (i % 2 === 0) {
       arraySubEx.push(subExpression());
@@ -90,16 +101,24 @@ function generateExp() {
   for(i =0; i< numberOfPairs; i++){
     arraySubEx = generateBracketPair(arraySubEx);
   }
-  return arraySubEx;
+  $problem.html(arraySubEx.join(' '));
+
+  return arraySubEx.join(' ');
 }
 
-  // console.log(generateExp());
-
-
-function ansGenerateExp() {
-  return eval(generateExp());
+function checkAnswer() {
+  const userAnswer = $input.val('');
+  const expressionAnswer = eval(generateExp()).toString();
+  if (userAnswer === expressionAnswer) {
+    stats++;
+    updateScore();
+  } else {
+    stats--;
+    updateScore();
+  }
 }
-ansGenerateExp();
-  // console.log(ansGenerateExp());
 
-// if starts and end with backets randomly assign!
+function updateScore() {
+  if (stats >= 0) $stats.html(stats);
+  $input.val('');
+}
